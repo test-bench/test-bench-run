f7216b846ab063d52ed10aa47502c545c89e86f2
Nathan Ladd <nathanladd@gmail.com>
2023-04-29 13:16:45 -0500

    File Output: Pend Event & Pended Event Predicate

--- lib/test_bench/run/output/file.rb
+++ lib/test_bench/run/output/file.rb
@@ -11,6 +11,29 @@ module TestBench
         end
         attr_writer :pended_events
 
+        def pend_event(event_data)
+          process_id = event_data.process_id
+
+          if not started?(process_id)
+            start(process_id)
+          end
+
+          pended_events = self.pended_events[process_id]
+          pended_events << event_data
+        end
+        alias :pend :pend_event
+
+        def pended_event?(event_data)
+          process_id = event_data.process_id
+
+          pended_events = self.pended_events.fetch(process_id) do
+            return false
+          end
+
+          pended_events.include?(event_data)
+        end
+        alias :pended? :pended_event?
+
         def start(process_id)
           if started?(process_id)
             raise Error, "Process already started (Process ID: #{process_id})"
