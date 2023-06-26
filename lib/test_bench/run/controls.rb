f245a776d7c7b5853d48a261e93b542f197d6370
Nathan Ladd <nathanladd@gmail.com>
2023-06-26 06:58:22 -0500

    Events

--- lib/test_bench/run/controls.rb
+++ lib/test_bench/run/controls.rb
@@ -1,1 +1,15 @@
 require 'test_bench/fixture/controls'
+
+require 'test_bench/run/controls/file'
+require 'test_bench/run/controls/exception'
+require 'test_bench/run/controls/process_id'
+require 'test_bench/run/controls/random'
+require 'test_bench/run/controls/result'
+require 'test_bench/run/controls/time'
+
+require 'test_bench/run/controls/events/event_data'
+require 'test_bench/run/controls/events/file_started'
+require 'test_bench/run/controls/events/file_finished'
+require 'test_bench/run/controls/events/file_crashed'
+require 'test_bench/run/controls/events/started'
+require 'test_bench/run/controls/events/finished'
