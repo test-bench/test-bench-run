1131cd301dbb5cec9118f6e2698eaf02c6b00f50
Nathan Ladd <nathanladd@gmail.com>
2023-07-11 09:58:44 -0500

    Summary Output: Test Finished

---
+++ test/automated/summary_output/test_finished/failure.rb
@@ -0,0 +1,44 @@
+require_relative '../../automated_init'
+
+context "Summary Output" do
+  context "Handle Test Finished Event" do
+    context "Failure" do
+      result = Controls::Result.failure
+      test_finished = Controls::Events::Session::TestFinished.example(result:)
+
+      output = Run::Output::Summary.new
+
+      output.handle(test_finished)
+
+      context "Tests Finished" do
+        tests_finished = output.tests_finished
+
+        comment tests_finished.inspect
+
+        test "Incremented" do
+          assert(tests_finished == 1)
+        end
+      end
+
+      context "Tests Failed" do
+        tests_failed = output.tests_failed
+
+        comment tests_failed.inspect
+
+        test "Incremented" do
+          assert(tests_failed == 1)
+        end
+      end
+
+      context "Tests Passed" do
+        tests_passed = output.tests_passed
+
+        comment tests_passed.inspect
+
+        test "Not incremented" do
+          assert(tests_passed == 0)
+        end
+      end
+    end
+  end
+end
