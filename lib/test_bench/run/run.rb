a6e1b972656766ebc7a71be87421e8f19f903786
Nathan Ladd <nathanladd@gmail.com>
2023-07-19 10:27:03 -0500

    Run: Substitute

--- lib/test_bench/run/run.rb
+++ lib/test_bench/run/run.rb
@@ -87,5 +87,29 @@ module TestBench
     def ran?
       path_sequence > 0
     end
+
+    module Substitute
+      def self.build
+        Run.build
+      end
+
+      class Run < Run
+        def self.build
+          new
+        end
+
+        def path?(path)
+          get_files.path?(path)
+        end
+
+        def set_result(result)
+          if result
+            session.record_assertion
+          else
+            session.record_failure
+          end
+        end
+      end
+    end
   end
 end
